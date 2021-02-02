5a945a5b313bbd76f9d014120916c212ca1084b2
Rugved Koshiya <rugvedkoshiya2002@gmail.com>
2021-02-02 16:16:18 +0530

    Project Uploaded

---
+++ script.js
@@ -0,0 +1,25 @@
+const quote = document.getElementById("quote");
+const quoteAuthor = document.getElementById("quoteAuthor");
+const newBtn = document.getElementById("newBtn");
+const api = "https://type.fit/api/quotes";
+let data = ""
+let loadedData = ""
+
+async function onLoad() {
+    data = await fetch(api);
+    loadedData = await data.json();
+    getNew();
+}
+
+async function getNew() {
+    quoteNum = Math.floor(Math.random() * loadedData.length+1);
+    quote.innerHTML = loadedData[quoteNum].text;
+    if(loadedData[quoteNum].author == null) {
+        quoteAuthor.innerHTML = "Unknown";
+    }else {
+        quoteAuthor.innerHTML = loadedData[quoteNum].author;
+    }
+}
+
+newBtn.addEventListener("click", getNew);
+onLoad();
